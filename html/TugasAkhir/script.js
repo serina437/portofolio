2ffe8ddde69ae6c81a69548461b4fd77f3b7f5a6
serina437 <srnanraszdh@gmail.com>
2024-11-19 13:40:59 +0700

    commit ke 3

---
+++ html/TugasAkhir/script.js
@@ -0,0 +1,47 @@
+
+        function toggleText() {
+
+            // Get all the elements from the page
+            let points =
+                document.getElementById("points");
+
+            let showMoreText =
+                document.getElementById("moreText");
+
+            let buttonText =
+                document.getElementById("textButton");
+
+            // If the display property of the dots 
+            // to be displayed is already set to 
+            // 'none' (that is hidden) then this 
+            // section of code triggers
+            if (points.style.display === "none") {
+
+                // Hide the text between the span
+                // elements
+                showMoreText.style.display = "none";
+
+                // Show the dots after the text
+                points.style.display = "inline";
+
+                // Change the text on button to 
+                // 'Show More'
+                buttonText.innerHTML = "Show More";
+            }
+
+            // If the hidden portion is revealed,
+            // we will change it back to be hidden
+            else {
+
+                // Show the text between the
+                // span elements
+                showMoreText.style.display = "inline";
+
+                // Hide the dots after the text
+                points.style.display = "none";
+
+                // Change the text on button
+                // to 'Show Less'
+                buttonText.innerHTML = "Show Less";
+            }
+        }
